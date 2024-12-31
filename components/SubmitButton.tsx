b6c4e4e2125df6300b24cd5722ec0ed0d47c4bb0
rohansinha01 <rohansinha01@gmail.com>
2024-12-31 14:05:08 -0500

    appwrite

---
+++ components/SubmitButton.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button } from './ui/button'
+
+interface ButtonProps {
+    isLoading: boolean,
+    className?: string,
+    children: React.ReactNode,
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const SubmitButton = ({ isLoading, className, children}: ButtonProps) => {
+  return (
+   <Button type="submit" disabled={isLoading} className={className ?? 'shad-primary-btn w-full'}>
+    {isLoading ? (
+        <div className='flex items-center gap-4'>
+            <Image
+                src="/assets/icons/loaders.svg"
+                alt="loader"
+                width={24}
+                height={24}
+                className="animate-spin"
+            />
+            Loading...
+        </div>
+    ): children}
+
+   </Button>
+  )
+}
+
+export default SubmitButton

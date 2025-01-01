8de9bee75ee6599d1d32f94421f0f49ba54c8980
rohansinha01 <rohansinha01@gmail.com>
2025-01-01 01:17:12 -0500

    register outline

--- app/patients/[userid]/register/page.tsx
+++ app/patients/[userid]/register/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='flex h-screen max-h-screen'>
+        <section className='remove-scrollbar container'>
+            <div className='sub-container max-w-[860px] flex-1 flex-col py-10'>
+                <Image 
+                    src="/assets/icons/logo-full.svg" 
+                    height={1000}
+                    width={1000}
+                    alt="carepulse"
+                    className='mb-12 h-10 w-fit'
+                    />
+            </div>
+        </section>
+    </div>
   )
 }
 

96d3b2667f97ef6e5a15103598b22ff3e645d178
rohansinha01 <rohansinha01@gmail.com>
2025-01-02 14:14:18 -0500

    register template

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
           <div className="text-14-regular mt-20 flex justify-between">
           <p className="justify-items-end texxt-dark-600 xl:text-left">
-            © 2024 CarePulse
+            © 2025 CarePulse
             </p>
             <Link href="/?admin=true" className="text-green-500">
             Admin
--- app/patients/[userid]/register/page.tsx
+++ app/patients/[userid]/register/page.tsx
@@ -1,21 +1,42 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+import RegisterForm from '@/components/forms/RegisterForm'
 
 const Register = () => {
   return (
-    <div className='flex h-screen max-h-screen'>
-        <section className='remove-scrollbar container'>
-            <div className='sub-container max-w-[860px] flex-1 flex-col py-10'>
-                <Image 
-                    src="/assets/icons/logo-full.svg" 
-                    height={1000}
-                    width={1000}
-                    alt="carepulse"
-                    className='mb-12 h-10 w-fit'
-                    />
-            </div>
-        </section>
-    </div>
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP VERIFICATION | PASSKEYMODAL */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image 
+          src="/assets/icons/logo-full.svg"
+          height={1000}
+          width={1000}
+          alt={"patient"} 
+          className="mb-12 h-10 w-fit"/>
+
+        <RegisterForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+          <p className="justify-items-end texxt-dark-600 xl:text-left">
+            © 2025 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+            Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      
+      <Image 
+      src="/assets/images/register-img.png"
+      height={1000}
+      width={1000}
+      alt="patient"
+      className="side-img max-w-[390]"
+      />
+      </div>
   )
 }
 

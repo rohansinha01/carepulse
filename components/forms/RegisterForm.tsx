687587e4063ad51b2e38e4cde454abbf56f9bb08
rohansinha01 <rohansinha01@gmail.com>
2025-01-05 18:32:29 -0500

    medical history

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -268,6 +268,39 @@ const  RegisterForm = ({ user }: {user: User }) => {
 
             />
         </div>
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+        <CustomFormField 
+            fieldType={FormFieldType.TEXTAREA}
+            control={form.control}
+            name="familyMedicalHistory"
+            label="Family Medical History"
+            placeholder="Heart Disease, Diabetes"
+
+            />
+
+        <CustomFormField 
+            fieldType={FormFieldType.TEXTAREA}
+            control={form.control}
+            name="pastMedicalHistory"
+            label="Past Medical History"
+            placeholder="No legs"
+
+            />
+        </div>
+
+        <section className="space-y-6">
+            <div className="mb-9 space-y-1">
+              <h2 className="sub-header">
+               Identification and Verification
+            </h2>  
+            </div>
+            
+
+        </section>
+
+        
+
         <div className="flex flex-col gap-6 xl:flex-row">
             
         </div>

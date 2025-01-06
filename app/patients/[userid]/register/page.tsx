895401c2ce0adfc5db77f4ff3c7f65e949c00176
rohansinha01 <rohansinha01@gmail.com>
2025-01-06 16:27:06 -0500

    form ui

--- app/patients/[userid]/register/page.tsx
+++ app/patients/[userid]/register/page.tsx
@@ -9,8 +9,8 @@ const Register = async ({ params: { userId } }: SearchParamProps ) => {
   return (
     <div className="flex h-screen max-h-screen">
       {/* TODO: OTP VERIFICATION | PASSKEYMODAL */}
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
+      <section className="remove-scrollbar container">
+        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
           <Image 
           src="/assets/icons/logo-full.svg"
           height={1000}
@@ -19,15 +19,10 @@ const Register = async ({ params: { userId } }: SearchParamProps ) => {
           className="mb-12 h-10 w-fit"/>
 
         <RegisterForm user={user} />
-
-          <div className="text-14-regular mt-20 flex justify-between">
-          <p className="justify-items-end texxt-dark-600 xl:text-left">
+          <p className="copyright py-12">
             © 2025 CarePulse
             </p>
-            <Link href="/?admin=true" className="text-green-500">
-            Admin
-            </Link>
-          </div>
+
         </div>
       </section>
 

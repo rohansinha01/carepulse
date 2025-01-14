af026a39cfaca99a57095c531a0938e1eff3aadf
rohansinha01 <rohansinha01@gmail.com>
2025-01-13 20:30:52 -0500

    success page logo

--- app/patients/[registerId]/new-appointment/success/page.tsx
+++ app/patients/[registerId]/new-appointment/success/page.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Success = () => {
   return (
-    <div>Success</div>
+    <div className='flex h-screen max-h-screen px-[5%]'>
+        <div className='success-img'>
+            <Link href='/'>
+                <Image 
+                  src="/assets/icons/logo-full.svg"
+                  height={1000}
+                  width={1000}
+                  alt='logo'
+                  className='h-10 w-fit'  />
+            </Link>
+        </div>
+    </div>
   )
 }
 

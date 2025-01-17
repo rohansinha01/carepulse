19b7afebb49eb0510c33a64c86978d760469b99f
rohansinha01 <rohansinha01@gmail.com>
2025-01-17 14:25:07 -0500

    admin verification opens

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -11,17 +11,31 @@ import {
     AlertDialogHeader,
     AlertDialogTitle,
   } from "@/components/ui/alert-dialog"
-  
-
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 const PasskeyModal = () => {
-
+    const router = useRouter()
     const [open, setOpen] = useState(true)
+
+    const closeModal = () => {
+        setOpen(false)
+        router.push('/')
+    }
+
     return (
         <AlertDialog open={open} onOpenChange={setOpen}>
         
         <AlertDialogContent className='shad-alert-dialog'>
         <AlertDialogHeader>
             <AlertDialogTitle className='flex items-start justify-between'>Admin Access Verification</AlertDialogTitle>
+            <Image 
+                src="/assets/icons/close.svg"
+                alt="close"
+                width={20}
+                height={20}
+                onClick={() => closeModal()}
+                className='cursor-pointer'
+                />
             <AlertDialogDescription>
             This action cannot be undone. This will permanently delete your account
             and remove your data from our servers.

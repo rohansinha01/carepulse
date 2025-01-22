17e04336cc10742040ddc7c0b704079657f5556c
rohansinha01 <rohansinha01@gmail.com>
2025-01-22 14:39:44 -0500

    admin Link

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Admin = () => {
   return (
-    <div>Admin</div>
+    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
+        <header className='admin-header'>
+            <Link href="/" className="cursor-pointer">
+            
+            </Link>
+        </header>
+    </div>
   )
 }
 

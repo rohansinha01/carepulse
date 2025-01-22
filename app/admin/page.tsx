db1b6e3582c7c714a17558e945456c13679881ea
rohansinha01 <rohansinha01@gmail.com>
2025-01-22 14:43:42 -0500

    admin page header

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
 
 const Admin = () => {
@@ -6,7 +7,13 @@ const Admin = () => {
     <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
         <header className='admin-header'>
             <Link href="/" className="cursor-pointer">
-            
+                <Image 
+                    src="/assets/icons/logo-full.svg"
+                    height={32}
+                    width={162}
+                    alt="Logo"
+                    className='h-8 w-fit'
+                    />
             </Link>
         </header>
     </div>

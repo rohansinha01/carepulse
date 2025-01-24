aecf79f340f9b3cca9b24d36a383b19a9d78a91b
rohansinha01 <rohansinha01@gmail.com>
2025-01-24 00:09:40 -0500

    added in real data

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,8 +2,10 @@ import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
 import { StatCard } from '@/components/StatCard'
+import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
 
-const Admin = () => {
+const Admin = async () => {
+    const appointments = await getRecentAppointmentList()
   return (
     <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
         <header className='admin-header'>
@@ -28,19 +30,19 @@ const Admin = () => {
             <section className='admin-stat'>
                 <StatCard 
                     type="appointments"
-                    count={5}
+                    count={appointments.scheduledCount}
                     label="Scheduled appointments"
                     icon="/assets/icons/appointments.svg"
                     />
                  <StatCard 
                     type="pending"
-                    count={10}
+                    count={appointments.pendingCount}
                     label="Pending appointments"
                     icon="/assets/icons/pending.svg"
                     />
                 <StatCard 
                     type="cancelled"
-                    count={2}
+                    count={appointments.cancelledCount}
                     label="Cancelled appointments"
                     icon="/assets/icons/cancelled.svg"
                     />

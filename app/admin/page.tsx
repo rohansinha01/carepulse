4b6ef39944d6d8a260559c4b6cb1775e4329908c
rohansinha01 <rohansinha01@gmail.com>
2025-01-25 14:57:22 -0500

    updated page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,8 +3,21 @@ import Image from 'next/image'
 import React from 'react'
 import { StatCard } from '@/components/StatCard'
 import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
-import DataTable from '@/components/table/DataTable'
-import {columns} from '@/components/table/columns'
+import {DataTable} from '@/components/table/DataTable'
+import {columns, Payment} from '@/components/table/columns'
+
+async function getData(): Promise<Payment[]> {
+    // Fetch data from your API here.
+    return [
+      {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+      },
+      // ...
+    ]
+  }
 
 const Admin = async () => {
     const appointments = await getRecentAppointmentList()

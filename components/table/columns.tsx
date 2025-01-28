24db0170d885b057dfa793aeeef9bf4b2e10a259
rohansinha01 <rohansinha01@gmail.com>
2025-01-28 17:38:33 -0500

    StatusBadge

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -12,6 +12,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import StatusBadge from "../StatusBadge"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -36,6 +37,11 @@ export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => (
+      <div className="min-w-[115px]">
+        <StatusBadge status={row.original.status}/>
+      </div>
+    )
   },
   {
     accessorKey: "email",

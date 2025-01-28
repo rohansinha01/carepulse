e8ea06affbf03ed15e67d19c251dd3c11fbc94f8
rohansinha01 <rohansinha01@gmail.com>
2025-01-28 17:41:02 -0500

    StatusBadge class name clsx

--- components/StatusBadge.tsx
+++ components/StatusBadge.tsx
@@ -1,8 +1,13 @@
+import clsx from 'clsx'
 import React from 'react'
 
-const StatusBadge = () => {
+const StatusBadge = ({ status}: { status: Status}) => {
   return (
-    <div>StatusBadge</div>
+    <div className={clsx('status-badge', {
+        'bg-green-600': status === 'scheduled',
+        'bg-blue-600': status === 'pending',
+        'bg-red-600': status === 'cancelled'
+    })}>StatusBadge</div>
   )
 }
 

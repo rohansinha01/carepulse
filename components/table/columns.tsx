4435350533e1d467459dccc0cc44d262173d9352
rohansinha01 <rohansinha01@gmail.com>
2025-01-31 17:22:44 -0500

    AppointmentModal

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -76,11 +76,23 @@ export const columns: ColumnDef<Payment>[] = [
   {
     id: "actions",
     header: () => <div className="pl-4">Actions</div>,
-    cell: ({ row }) => {
+    cell: ({ row: { original: data} }) => {
       return (
         <div className="flex gap-1">
-          <AppointmentModal type="schedule"/>
-          <AppointmentModal type="cancel"/>
+          <AppointmentModal 
+          type="schedule"
+          patientId={data.patient.$id}
+          registerId={data.registerId}
+          appointmentId={data}
+          title="Schedule Appointment"
+          description="Please confirm the following details to schedule"/>
+          <AppointmentModal 
+            type="cancel"
+            patientId={data.patient.$id}
+            registerId={data.registerId}
+            appointmentId={data}
+            title="Cancel Appointment"
+            description="Are you sure you want to cancel this appointment"/>
         </div>
       )
  

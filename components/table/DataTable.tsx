f439885f6778b2d85bc52052b7b4973534a37687
rohansinha01 <rohansinha01@gmail.com>
2025-02-01 17:33:05 -0500

    className updated

--- components/table/DataTable.tsx
+++ components/table/DataTable.tsx
@@ -79,7 +79,7 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
-      <div className="flex items-center justify-end space-x-2 py-4">
+      <div className="table-actions">
         <Button
           variant="outline"
           size="sm"

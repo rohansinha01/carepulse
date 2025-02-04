51c7862f8b8911e84ec7849133531096cebd0760
rohansinha01 <rohansinha01@gmail.com>
2025-02-04 13:42:51 -0500

    updating actions

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -13,6 +13,7 @@ import {
   messaging,
 } from "../appwrite.config";
 import { formatDateTime, parseStringify } from "../utils";
+import { CreateAppointmentParams, UpdateAppointmentParams } from "@/types";
 
 //  CREATE APPOINTMENT
 export const createAppointment = async (

62252510023394aa968532270fc7166b2ae61568
rohansinha01 <rohansinha01@gmail.com>
2025-02-04 09:39:36 -0500

    npm updated

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ import Image from "next/image"
 import Link from "next/link";
 import React from "react"
 
-export const Home = async ({ searchParams }: SearchParamProps) => {
-  const isAdmin = (await searchParams)?.admin === "true";
+export default function Home({ searchParams}: SearchParamProps) {
+  const isAdmin = searchParams?.admin === 'true'
   return (
     <span className="flex h-screen max-h-screen">
       {isAdmin && <PasskeyModal />}
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,8 +2,8 @@
 /* eslint-disable no-unused-vars */
 
 declare type SearchParamProps = {
-  params: Promise<{ [key: string]: string }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+  params: { [key: string]: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
   
   declare type Gender = "Male" | "Female" | "Other";

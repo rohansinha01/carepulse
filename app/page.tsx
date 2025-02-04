a6064d395472816faade9eb8afce26a57adb5e5a
rohansinha01 <rohansinha01@gmail.com>
2025-02-04 11:35:01 -0500

    trying adding NextPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,50 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextPage } from "next";
 import PatientForm from "@/components/forms/PatientForm";
 import { PasskeyModal } from "@/components/PasskeyModal";
-
-import Image from "next/image"
+import Image from "next/image";
 import Link from "next/link";
-import React from "react"
 
-interface PageProps {
-  searchParams: { admin?: string };
+interface HomeProps {
+  searchParams?: { admin?: string };
 }
 
-export default function Home({ searchParams }: PageProps) {
+const Home: NextPage<HomeProps> = ({ searchParams }) => {
   const isAdmin = searchParams?.admin === "true";
+
   return (
     <span className="flex h-screen max-h-screen">
       {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image 
-          src="/assets/icons/logo-full.svg"
-          height={1000}
-          width={1000}
-          alt={"patient"} 
-          className="mb-12 h-10 w-fit"/>
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt={"patient"} 
+            className="mb-12 h-10 w-fit"
+          />
 
           <PatientForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
-          <p className="justify-items-end texxt-dark-600 xl:text-left">
-            © 2025 CarePulse
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2025 CarePulse
             </p>
             <Link href="/?admin=true" className="text-green-500">
-            Admins Click Here
-          
+              Admins Click Here
             </Link>
           </div>
         </div>
       </section>
       <Image 
-      src="/assets/images/onboarding-img.png"
-      height={1000}
-      width={1000}
-      alt="patient"
-      className="side-img max-w-[50%]"
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
       />
-      </span>
+    </span>
   );
-  
-}
+};
 
-
-
+export default Home;

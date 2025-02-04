9c04ca1487ac1e268e0f5e993120a9710bac52ee
rohansinha01 <rohansinha01@gmail.com>
2025-02-04 15:25:32 -0500

    updating timeZone

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 

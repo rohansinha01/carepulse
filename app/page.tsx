ddc3acd9f8bea8581d289c22b2fae23c592f472f
rohansinha01 <rohansinha01@gmail.com>
2025-02-04 11:54:25 -0500

    reverted back to old code

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,23 @@
-import PatientForm from "@/components/forms/PatientForm";
-import { PasskeyModal } from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-interface PageProps {
-  searchParams?: { admin?: string };
-}
+import PatientForm from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
 
-export default function Home({ searchParams }: PageProps) {
+const Home = ({ searchParams }: SearchParamProps) => {
   const isAdmin = searchParams?.admin === "true";
 
   return (
-    <span className="flex h-screen max-h-screen">
+    <div className="flex h-screen max-h-screen">
       {isAdmin && <PasskeyModal />}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-          <Image 
+          <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
             width={1000}
-            alt={"patient"} 
+            alt="patient"
             className="mb-12 h-10 w-fit"
           />
 
@@ -27,21 +25,24 @@ export default function Home({ searchParams }: PageProps) {
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2025 CarePulse
+              © 2025 CarePluse
             </p>
             <Link href="/?admin=true" className="text-green-500">
-              Admins Click Here
+              Admin
             </Link>
           </div>
         </div>
       </section>
-      <Image 
+
+      <Image
         src="/assets/images/onboarding-img.png"
         height={1000}
         width={1000}
         alt="patient"
         className="side-img max-w-[50%]"
       />
-    </span>
+    </div>
   );
-}
+};
+
+export default Home;
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,57 +1,57 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-unused-vars */
 
-type PageProps = {
-  searchParams?: { [key: string]: string | undefined };
+declare type SearchParamProps = {
+  params: { [key: string]: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
 
-  
-  declare type Gender = "Male" | "Female" | "Other";
-  declare type Status = "pending" | "scheduled" | "cancelled";
-  
-  declare interface CreateUserParams {
-    name: string;
-    email: string;
-    phone: string;
-  }
-  declare interface User extends CreateUserParams {
-    $id: string;
-  }
-  
-  declare interface RegisterUserParams extends CreateUserParams {
-    registerId: string;
-    birthDate: Date;
-    gender: Gender;
-    address: string;
-    occupation: string;
-    emergencyContactName: string;
-    emergencyContactNumber: string;
-    primaryPhysician: string;
-    insuranceProvider: string;
-    insurancePolicyNumber: string;
-    allergies: string | undefined;
-    currentMedication: string | undefined;
-    familyMedicalHistory: string | undefined;
-    pastMedicalHistory: string | undefined;
-    identificationType: string | undefined;
-    identificationNumber: string | undefined;
-    identificationDocument: FormData | undefined;
-    privacyConsent: boolean;
-  }
-  
-  declare type CreateAppointmentParams = {
-    registerId: string;
-    patient: string;
-    primaryPhysician: string;
-    reason: string;
-    schedule: Date;
-    status: Status;
-    note: string | undefined;
-  };
-  
-  declare type UpdateAppointmentParams = {
-    appointmentId: string;
-    registerId: string;
-    appointment: Appointment;
-    type: string;
-  };
+declare type Gender = "Male" | "Female" | "Other";
+declare type Status = "pending" | "scheduled" | "cancelled";
+
+declare interface CreateUserParams {
+  name: string;
+  email: string;
+  phone: string;
+}
+declare interface User extends CreateUserParams {
+  $id: string;
+}
+
+declare interface RegisterUserParams extends CreateUserParams {
+  registerId: string;
+  birthDate: Date;
+  gender: Gender;
+  address: string;
+  occupation: string;
+  emergencyContactName: string;
+  emergencyContactNumber: string;
+  primaryPhysician: string;
+  insuranceProvider: string;
+  insurancePolicyNumber: string;
+  allergies: string | undefined;
+  currentMedication: string | undefined;
+  familyMedicalHistory: string | undefined;
+  pastMedicalHistory: string | undefined;
+  identificationType: string | undefined;
+  identificationNumber: string | undefined;
+  identificationDocument: FormData | undefined;
+  privacyConsent: boolean;
+}
+
+declare type CreateAppointmentParams = {
+  registerId: string;
+  patient: string;
+  primaryPhysician: string;
+  reason: string;
+  schedule: Date;
+  status: Status;
+  note: string | undefined;
+};
+
+declare type UpdateAppointmentParams = {
+  appointmentId: string;
+  registerId: string;
+  timeZone: string;
+  appointment: Appointment;
+  type: string;
+};
